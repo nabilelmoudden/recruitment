22586b8faf51f95d149936aaaceedcc14422eb8a
nabil el moudden <nabilelmoudden@gmail.com>
2019-10-28 10:11:20 +0100

    Update BookRepository.java

--- src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
+++ src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
@@ -2,6 +2,7 @@ package fr.d2factory.libraryapp.book;
 
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -9,25 +10,67 @@ import java.util.Map;
  * The book repository emulates a database via 2 HashMaps
  */
 public class BookRepository {
-	 private Map<ISBN, Book> availableBooks = new HashMap<>();
-	    private Map<ISBN, LocalDate> borrowedBooks = new HashMap<>();
+    private Map<ISBN, Book> availableBooks = new HashMap<>();
+    private Map<Book, LocalDate> borrowedBooks = new HashMap<>();
 
-	    public void addBooks(List<Book> books){
-	        for (Book book : books) {
-	            availableBooks.put(book.getIsbn(), book);
-	        }
-	    }
+    public void addBooks(List<Book> books){
+    	 if (books != null && !books.isEmpty()) {
 
-	    public Book findBook(long isbnCode) {
-	        ISBN foo = new ISBN(isbnCode);
-	        return availableBooks.get(foo);
-	    }
+             books.stream().forEach(book -> {
+                 if (book != null) {
+                     availableBooks.put(book.getIsbn(), book);
+                 }
+             });
+         }
+    }
 
-	    public void saveBookBorrow(Book book, LocalDate borrowedAt){
-	        borrowedBooks.put(availableBooks.remove(book.getIsbn()).getIsbn(), borrowedAt);
-	    }
+    public Book findBook(long isbnCode) {
+    	 return this.availableBooks.entrySet().stream().filter(v -> v.getKey().getIsbnCode() ==
+                 isbnCode).map(Map.Entry::getValue)
+                 .findFirst()
+                 .orElse(null);
 
-	    public LocalDate findBorrowedBookDate(ISBN isbn) {
-	        return borrowedBooks.get(isbn);
-	    }
+    }
+
+    public void saveBookBorrow(Book book, LocalDate borrowedAt){
+    	borrowedBooks.put(book, borrowedAt);
+    }
+
+    public LocalDate findBorrowedBookDate(Book book) {
+    	if(book!= null) {
+    		 return this.borrowedBooks.entrySet().stream().filter(b -> b.getKey().equals(book)).map(Map.Entry::getValue)
+    	                .findFirst()
+    	                .orElse(null);
+
+		}
+		return null;
+    }
+    
+	public Book removeBook(long isbnCode) {		
+		Iterator<Map.Entry<ISBN, Book>> iterator = availableBooks.entrySet().iterator();
+		 while (iterator.hasNext()) {
+			Map.Entry<ISBN, Book> entry = iterator.next();
+			if (entry.getKey().getIsbnCode() == isbnCode ) {
+				availableBooks.remove(entry.getKey());
+				return entry.getValue();				
+			}
+		}
+		return null;		
 	}
+
+	public Map<ISBN, Book> getAvailableBooks() {
+		return availableBooks;
+	}
+
+	public void setAvailableBooks(Map<ISBN, Book> availableBooks) {
+		this.availableBooks = availableBooks;
+	}
+
+	public Map<Book, LocalDate> getBorrowedBooks() {
+		return borrowedBooks;
+	}
+
+	public void setBorrowedBooks(Map<Book, LocalDate> borrowedBooks) {
+		this.borrowedBooks = borrowedBooks;
+	}
+}
